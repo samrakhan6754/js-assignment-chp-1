13bb9ada466474093a5841a55f587400b12756d9
samrakhan6754 <143213626+samrakhan6754@users.noreply.github.com>
2023-10-14 14:28:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+// Practice exercise 1.1
+
+// 1. Open the browser console, type 4 + 10, and press Enter. What do you see as
+// the response?
+
+14
+
+// 2. Use the console.log() syntax, placing a value within the rounded brackets.
+// Try entering your name with quotes around it (this is to indicate the fact that
+// it's a text string—we'll get to this in the next chapter).
+
+
+console.log("samra khan");
+
+// Practice exercise 1.2
+// JavaScript in an HTML page:
+
+// 1. Open your code editor and create an HTML file.
+
+// 2. Within your HTML file, set up the HTML tags, doctype, HTML, head, and
+// body, and then proceed and add the script tags.
+
+// 3. Place some JavaScript code within the script tags. You can use console.
+// log("hello world!")
+
+// Practice exercise 1.3
+// Linking to a JS JavaScript file:
+
+// 1. Create a separate file called app with the extension .js.
+
+// 2. Within the .js file, add some JavaScript code.
+
+// app.js
+
+console.log("working on app js");
+
+
+// 3. Link to the separate .js file within the HTML file you created in Practice
+// exercise 1.2.
+
+// 4. Open the HTML file within your browser and check to see whether the
+// JavaScript code ran properly.
+
+
+
+
+// Practice exercise 1.4
+// Adding comments:
+
+//  1. Add a new statement to your JavaScript code by setting a variable value.
+//  Since we will cover this in the next chapter, you can use the following line:
+// let a = 10;
+
+let a = 10;
+
+
+// 2. Add a comment at the end of the statement indicating that you set a value of
+//  10. 
+
+
+
+
+// 3. Print the value using console.log(). Add a comment explaining what this
+// will do.
+// Getting Started with JavaScript
+// [ 14 ]
+
+console.log(a); // Print the value of 'a'
+
+// 4. At the end of your JavaScript code, use a multiple-line comment. In a real
+// production script, you might use this space to add a brief outline of the
+// purpose of the file
+
+
+/*
+   This file contains JavaScript code for practice exercises,
+    illustrating various JavaScript concepts.
+*/
+// Self-check quiz
+
+// 1. What is the HTML syntax to add an external JavaScript file?
+
+//    To add an external JavaScript file to an HTML document, you use the `<script>` tag with the `src` attribute. Here's the syntax:
+
+//    ```html
+//    <script src="external.js"></script>
+//    ```
+
+//    Replace `"external.js"` with the actual path to your JavaScript file.
+
+// 2. **Can you run JavaScript in a file with a JS extension in your browser?**
+
+//    No, you cannot directly run a JavaScript file (with a `.js` extension) in a web browser like you do with HTML files.
+//     JavaScript files are meant to be included in HTML files using the `<script>` tag.  You run JavaScript code by including it within an HTML file and then opening that HTML file in
+//      a web browser.
+  
+
+// 3. **How do you write a multiple-line comment in JavaScript?**
+
+//    You can write a multiple-line comment in JavaScript using `/*` to begin the comment block and `*/` to end it. Here's an example:
+
+//    ```javascript
+//    /*
+//       This is a multiple-line comment.
+//       It can span multiple lines.
+//    */
+//    ```
+
+// 4. What is the best way to remove a line of code from running that you might want to keep as you debug?
+
+//    The best way to temporarily disable a line of code from running while debugging is to comment it out. 
+//    You can use `//` to comment out a single line or `/* */` to comment out multiple lines. 
+
+//    Single-line comment:
+
+//    ```javascript
+//    // This line of code won't run
+//    ```
+
+//    Multiple-line comment:
+
+//    ```javascript
+//    /*
+//       This block of code won't run.
+//       You can comment out multiple lines.
+//    */
+//    ```
+
+ 
